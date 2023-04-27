ffe66153869c75385b9388c15da748119e50f77a
Irmica <128592858+Irmica@users.noreply.github.com>
2023-04-27 16:12:56 +0300

    Добавлены функции создания и вывода массива в консоль. 1.CreateArray. 2.ShowArray.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i += 1)
+    {
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i += 1)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
